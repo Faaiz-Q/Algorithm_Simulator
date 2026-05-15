6ebafd7f6c888f889e41fc3dfa44e7c661651ce7
faaiz <219891015+Faaiz-Q@users.noreply.github.com>
2026-05-15 20:14:35 +0300

    added selection sort

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const sortBtn = document.getElementById("sort");
 const speedSlider = document.getElementById("speed");
 const sizeSlider = document.getElementById("size");
 
+const algorithmSelect=document.getElementById("algorithm");
+
 let arr = [];
 
 function generateArray(){
@@ -68,7 +70,53 @@ async function bubbleSort(){
     sortBtn.disabled = false;
 }
 
-sortBtn.addEventListener("click", bubbleSort);
+
+async function selectionSort(){
+    generateBtn.disabled = true;
+    sortBtn.disabled = true;
+
+    const bars = document.querySelectorAll(".bar");
+
+    for(let i=0;i<arr.length;i++){
+        let minIndex = i;
+        bars[minIndex].style.backgroundColor = "blue";
+
+        for(let j=i+1;j<arr.length;j++){
+            bars[j].style.backgroundColor = "blue";
+
+            await sleep(1000-speedSlider.value);
+
+            if(arr[j]<arr[minIndex])
+            {
+                bars[minIndex].style.backgroundColor = "maroon";
+                minIndex = j;
+                bars[minIndex].style.backgroundColor = "blue";
+            }
+            else{
+                bars[j].style.backgroundColor = "maroon";
+            }
+        }
+        [arr[minIndex],arr[i]] = [arr[i],arr[minIndex]];
+
+            bars[minIndex].style.height = `${arr[minIndex]}px`;
+            bars[i].style.height = `${arr[i]}px`;
+
+            bars[minIndex].style.backgroundColor = "maroon";
+            bars[i].style.backgroundColor = "green";
+    }
+    generateBtn.disabled = false;
+    sortBtn.disabled = false;
+}
+
+sortBtn.addEventListener("click",()=>{
+    const algorithm = algorithmSelect.value;
+    if(algorithm === "bubble"){
+        bubbleSort();
+    }
+    else if(algorithm === "selection"){
+        selectionSort();
+    }
+});
 generateBtn.addEventListener("click",generateArray);
 sizeSlider.addEventListener("input", generateArray);
 
